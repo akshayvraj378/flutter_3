8e6e1115169f798a801184fbec041d764f528efa
Vijayleo46 <vijaymartin72@gmail.com>
2023-10-30 10:57:19 +0530

    done

---
+++ android/app/src/main/kotlin/com/akshay/flutter_3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.akshay.flutter_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body:Center(child: Text('hello')
+      ),
+    ));
+  }
+}
